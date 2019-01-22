cef4f669a68394d3c309af0db5fdadec184e6865
Jason <jmherning93@gmail.com>
2019-01-22 09:16:35 -0900

    paddles and ball.

---
+++ pong.py
@@ -0,0 +1,51 @@
+# turtle pong
+# by Jason Herning
+
+
+
+import turtle
+
+
+window = turtle.Screen()
+window.title("Turtle Pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+
+# Paddle A
+paddleA = turtle.Turtle()
+paddleA.speed(0)
+paddleA.shape("square")
+paddleA.color("white")
+paddleA.shapesize(stretch_wid=5, stretch_len=1)
+paddleA.penup()
+paddleA.goto(-350, 0)
+
+
+# Paddle B
+paddleB = turtle.Turtle()
+paddleB.speed(0)
+paddleB.shape("square")
+paddleB.color("white")
+paddleB.shapesize(stretch_wid=5, stretch_len=1)
+paddleB.penup()
+paddleB.goto(350, 0)
+
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
+
+
+
+
+# Main game loop
+while True:
+    window.update()
